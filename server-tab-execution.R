15cd7201e0c5f09f7f24ba13894413ec0a6e1ec4
victor-ramos <victorramos@einstein.rockefeller.edu>
2021-11-06 13:16:14 -0400

    docker test

--- server-tab-execution.R
+++ server-tab-execution.R
@@ -1,4 +1,4 @@
-rv = reactiveValues(tsv.file = "", done = 0, folder.name = "", databases.folder = "/root/GitHub/VGene/database/" )
+rv = reactiveValues(tsv.file = "", done = 0, folder.name = "", databases.folder = "/home/developer/GitHub/VGene/database/" )
 
 rename.files = function (x, folder.name){
     file.copy(from = x[4], to = paste0(folder.name, x[1]))
